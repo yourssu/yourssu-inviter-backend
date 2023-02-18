536e65fa0e68f974a410a1ccbccbbfcfdfcf7381
citrusinesis <jihojiho2003@gmail.com>
2023-02-18 16:30:34 +0900

    create Invitation

---
+++ src/main/kotlin/com/example/yourssuinviterbackend/domain/invitation/entity/Invitation.kt
@@ -0,0 +1,42 @@
+package com.example.yourssuinviterbackend.domain.invitation.entity
+
+import com.example.yourssuinviterbackend.BaseTimeEntity
+import org.hibernate.annotations.Type
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@Entity
+@Table(name = "invitations")
+class Invitation (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "invitation_id", nullable = false)
+    val id: Long,
+
+    @Column(name = "password", nullable = false)
+    val password: String,
+
+    @Column(name = "title", nullable = false)
+    val title: String,
+
+    @Lob
+    @Column(name = "image", columnDefinition = "MEDIUMBLOB")
+    val image: ByteArray,
+
+    @Column(name = "description", nullable = false)
+    val description: String,
+
+    @Column(name = "date", nullable = false)
+    val date: LocalDateTime,
+
+    @Column(name = "location", nullable = false)
+    val location: String,
+
+    @Type(type = "json")
+    @Column(name = "extra", columnDefinition = "json")
+    val extra: List<Map<String, Any>>,
+
+    @Type(type = "json")
+    @Column(name = "form_data", columnDefinition = "json")
+    val formData: List<Map<String, Any>>
+) : BaseTimeEntity()
