2c1296338cf52eaf5fa96bb0747b851043213de4
HwanGonJang <myggona@gmail.com>
2023-02-20 16:30:11 +0900

    refactor: make json nullable

--- src/main/kotlin/com/example/yourssuinviterbackend/common/util/JsonConverterUtil.kt
+++ src/main/kotlin/com/example/yourssuinviterbackend/common/util/JsonConverterUtil.kt
@@ -4,7 +4,8 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
 object JsonConverterUtil {
-    fun jsonToMap(json: String?): Map<String, String> {
+    fun jsonToMap(json: String?): Map<String, String>? {
+        if(json == null) return null
         val mapType = object : TypeToken<Map<String, String>>() {}.type
         return Gson().fromJson(json, mapType)
     }
